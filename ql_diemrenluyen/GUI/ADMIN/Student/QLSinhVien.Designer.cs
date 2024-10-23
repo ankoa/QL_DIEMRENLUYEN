101b527e4fbeba7fa5f8ebccf40b04616d59a5ae
nntdai <114330158+nntdai@users.noreply.github.com>
2024-10-23 22:42:20 +0700

    load student

--- ql_diemrenluyen/GUI/ADMIN/Student/QLSinhVien.Designer.cs
+++ ql_diemrenluyen/GUI/ADMIN/Student/QLSinhVien.Designer.cs
@@ -31,10 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QLSinhVien));
             label1 = new Label();
             panel1 = new Panel();
+            panel2 = new Panel();
+            label2 = new Label();
             button2 = new Button();
             btnSearch = new Button();
             textBox1 = new TextBox();
-            dataGridView1 = new DataGridView();
+            dataGridStudent = new DataGridView();
             maSV = new DataGridViewTextBoxColumn();
             Column1 = new DataGridViewTextBoxColumn();
             gioiTinh = new DataGridViewTextBoxColumn();
@@ -42,11 +44,9 @@
             khoa = new DataGridViewTextBoxColumn();
             btnXemTT = new DataGridViewButtonColumn();
             btnXemDiem = new DataGridViewButtonColumn();
-            label2 = new Label();
-            panel2 = new Panel();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridStudent).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -55,9 +55,9 @@
             label1.BackColor = SystemColors.MenuHighlight;
             label1.Font = new Font("Segoe UI Black", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ControlLightLight;
-            label1.Location = new Point(0, 12);
+            label1.Location = new Point(0, 9);
             label1.Name = "label1";
-            label1.Size = new Size(331, 50);
+            label1.Size = new Size(270, 41);
             label1.TabIndex = 0;
             label1.Text = "Quản lý sinh viên";
             label1.Click += label1_Click;
@@ -70,20 +70,45 @@
             panel1.Controls.Add(button2);
             panel1.Controls.Add(btnSearch);
             panel1.Controls.Add(textBox1);
-            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(dataGridStudent);
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1057, 657);
+            panel1.Size = new Size(1165, 493);
             panel1.TabIndex = 1;
             panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.MenuHighlight;
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(962, 53);
+            panel2.TabIndex = 6;
+            panel2.Paint += panel2_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(64, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(230, 32);
+            label2.TabIndex = 5;
+            label2.Text = "Danh sách sinh viên";
+            label2.Click += label2_Click;
             // 
             // button2
             // 
             button2.BackColor = SystemColors.ButtonFace;
             button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.Location = new Point(837, 90);
+            button2.Location = new Point(732, 68);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(90, 50);
+            button2.Size = new Size(79, 38);
             button2.TabIndex = 4;
             button2.UseVisualStyleBackColor = false;
             // 
@@ -91,9 +116,10 @@
             // 
             btnSearch.BackColor = SystemColors.ButtonFace;
             btnSearch.Image = (Image)resources.GetObject("btnSearch.Image");
-            btnSearch.Location = new Point(780, 90);
+            btnSearch.Location = new Point(683, 68);
+            btnSearch.Margin = new Padding(3, 2, 3, 2);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(51, 50);
+            btnSearch.Size = new Size(45, 38);
             btnSearch.TabIndex = 3;
             btnSearch.UseVisualStyleBackColor = false;
             btnSearch.Click += button1_Click;
@@ -102,29 +128,34 @@
             // 
             textBox1.BackColor = SystemColors.ButtonFace;
             textBox1.Font = new Font("Segoe UI", 18F);
-            textBox1.Location = new Point(451, 90);
+            textBox1.Location = new Point(395, 68);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(380, 47);
+            textBox1.Size = new Size(333, 39);
             textBox1.TabIndex = 2;
             textBox1.Text = "\r\n";
             textBox1.TextChanged += textBox1_TextChanged;
             // 
-            // dataGridView1
+            // dataGridStudent
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { maSV, Column1, gioiTinh, lop, khoa, btnXemTT, btnXemDiem });
-            dataGridView1.Location = new Point(21, 146);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(930, 493);
-            dataGridView1.TabIndex = 0;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            dataGridStudent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridStudent.Columns.AddRange(new DataGridViewColumn[] { maSV, Column1, gioiTinh, lop, khoa, btnXemTT, btnXemDiem });
+            dataGridStudent.Location = new Point(14, 110);
+            dataGridStudent.Margin = new Padding(3, 2, 3, 2);
+            dataGridStudent.MultiSelect = false;
+            dataGridStudent.Name = "dataGridStudent";
+            dataGridStudent.ReadOnly = true;
+            dataGridStudent.RowHeadersWidth = 51;
+            dataGridStudent.Size = new Size(933, 370);
+            dataGridStudent.TabIndex = 0;
+            dataGridStudent.CellContentClick += dataGridView1_CellContentClick;
             // 
             // maSV
             // 
             maSV.HeaderText = "Mã sinh viên";
             maSV.MinimumWidth = 6;
             maSV.Name = "maSV";
+            maSV.ReadOnly = true;
             maSV.Width = 125;
             // 
             // Column1
@@ -132,6 +163,7 @@
             Column1.HeaderText = "Họ và Tên";
             Column1.MinimumWidth = 6;
             Column1.Name = "Column1";
+            Column1.ReadOnly = true;
             Column1.Width = 125;
             // 
             // gioiTinh
@@ -139,6 +171,7 @@
             gioiTinh.HeaderText = "Giới tính ";
             gioiTinh.MinimumWidth = 6;
             gioiTinh.Name = "gioiTinh";
+            gioiTinh.ReadOnly = true;
             gioiTinh.Width = 125;
             // 
             // lop
@@ -146,6 +179,7 @@
             lop.HeaderText = "Lớp";
             lop.MinimumWidth = 6;
             lop.Name = "lop";
+            lop.ReadOnly = true;
             lop.Width = 125;
             // 
             // khoa
@@ -153,6 +187,7 @@
             khoa.HeaderText = "Khoa";
             khoa.MinimumWidth = 6;
             khoa.Name = "khoa";
+            khoa.ReadOnly = true;
             khoa.Width = 125;
             // 
             // btnXemTT
@@ -160,6 +195,7 @@
             btnXemTT.HeaderText = "Xem Thông tin";
             btnXemTT.MinimumWidth = 6;
             btnXemTT.Name = "btnXemTT";
+            btnXemTT.ReadOnly = true;
             btnXemTT.Text = "Xem";
             btnXemTT.UseColumnTextForButtonValue = true;
             btnXemTT.Width = 125;
@@ -169,44 +205,25 @@
             btnXemDiem.HeaderText = "Xem / Sửa điểm";
             btnXemDiem.MinimumWidth = 6;
             btnXemDiem.Name = "btnXemDiem";
+            btnXemDiem.ReadOnly = true;
             btnXemDiem.Text = "Xem";
+            btnXemDiem.UseColumnTextForButtonValue = true;
             btnXemDiem.Width = 125;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.ActiveCaptionText;
-            label2.Location = new Point(73, 90);
-            label2.Name = "label2";
-            label2.Size = new Size(288, 41);
-            label2.TabIndex = 5;
-            label2.Text = "Danh sách sinh viên";
-            label2.Click += label2_Click;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = SystemColors.MenuHighlight;
-            panel2.Controls.Add(label1);
-            panel2.Location = new Point(0, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(996, 71);
-            panel2.TabIndex = 6;
-            panel2.Paint += panel2_Paint;
             // 
             // QLSinhVien
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "QLSinhVien";
-            Size = new Size(996, 662);
+            Size = new Size(962, 496);
             Load += QLSinhVien_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridStudent).EndInit();
             ResumeLayout(false);
         }
 
@@ -214,7 +231,12 @@
 
         private Label label1;
         private Panel panel1;
-        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Button btnSearch;
+        private Button button2;
+        private Label label2;
+        private Panel panel2;
+        private DataGridView dataGridStudent;
         private DataGridViewTextBoxColumn maSV;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn gioiTinh;
@@ -222,10 +244,5 @@
         private DataGridViewTextBoxColumn khoa;
         private DataGridViewButtonColumn btnXemTT;
         private DataGridViewButtonColumn btnXemDiem;
-        private TextBox textBox1;
-        private Button btnSearch;
-        private Button button2;
-        private Label label2;
-        private Panel panel2;
     }
 }
--- ql_diemrenluyen/GUI/ADMIN/Student/QLSinhVien.cs
+++ ql_diemrenluyen/GUI/ADMIN/Student/QLSinhVien.cs
@@ -1,11 +1,35 @@
-﻿namespace QLDiemRenLuyen
+﻿using System.Windows.Forms;
+using ql_diemrenluyen.BUS;
+using ql_diemrenluyen.DTO;
+
+namespace QLDiemRenLuyen
 {
     public partial class QLSinhVien : UserControl
     {
+        private static SinhVienBUS SinhVienBUS = new SinhVienBUS();
+        private static List<SinhVienDTO> listStudent = new List<SinhVienDTO>();
         public QLSinhVien()
         {
             InitializeComponent();
+            listStudent = SinhVienBUS.GetAllStudents();
+            listStudent.ForEach(student => {
+                var index = this.dataGridStudent.Rows.Add();
+                this.dataGridStudent.Rows[index].Cells[0].Value = student.Id;
+                this.dataGridStudent.Rows[index].Cells[1].Value = student.Name;
+                this.dataGridStudent.Rows[index].Cells[2].Value = student.GioiTinh;
+                this.dataGridStudent.Rows[index].Cells[3].Value = student.LopId;
+                this.dataGridStudent.Rows[index].Cells[4].Value = "Baqar";
+            });
 
+            
+            //dataGridStudent.DataSource = listStudent;
+            //autoresize each column
+
+            dataGridStudent.AutoResizeColumn(0);
+
+            dataGridStudent.AutoResizeColumn(1);
+
+            dataGridStudent.AutoResizeColumn(2);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -47,5 +71,7 @@
         {
 
         }
+
+       
     }
 }
