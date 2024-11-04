5d34aa8c809bd0860e2197cbb3e3026abcfd23f3
minhthai342003 <thaidang0304@gmail.com>
2024-11-04 17:08:10 +0700

    Add files via upload

---
+++ ql_diemrenluyen/BUS/GiangVienBUS.cs
@@ -0,0 +1,42 @@
+﻿using ql_diemrenluyen.DAO;
+using ql_diemrenluyen.DTO;
+using System.Collections.Generic;
+
+namespace ql_diemrenluyen.BUS
+{
+    public class GiangVienBUS
+    {
+        // Lấy tất cả giảng viên
+        public static List<GiangVienDTO> GetAllGiangVien()
+        {
+            return GiangVienDAO.GetAllGiangVien();
+        }
+
+        // Thêm giảng viên mới
+        public static bool AddGiangVien(GiangVienDTO giangVien)
+        {
+            return GiangVienDAO.AddGiangVien(giangVien);
+        }
+
+        // Cập nhật thông tin giảng viên
+        public static bool UpdateGiangVien(GiangVienDTO giangVien)
+        {
+            return GiangVienDAO.UpdateGiangVien(giangVien);
+        }
+
+        // Xóa giảng viên
+        public static bool DeleteGiangVien(long id)
+        {
+            return GiangVienDAO.DeleteGiangVien(id);
+        }
+        public static GiangVienDTO GetGiangVienById(long id)
+        {
+            return GiangVienDAO.GetGiangVienById(id);
+        }
+        public static List<GiangVienDTO> SearchGiangVien(string searchTerm)
+        {
+            return GiangVienDAO.SearchGiangVien(searchTerm);
+        }
+
+    }
+}
