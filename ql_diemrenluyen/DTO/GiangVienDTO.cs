c4205294e7c6c26cb9cdf519981ea4da1d85f13a
minhthai342003 <thaidang0304@gmail.com>
2024-11-04 17:09:38 +0700

    Add files via upload

---
+++ ql_diemrenluyen/DTO/GiangVienDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ql_diemrenluyen.DTO
+{
+    public class GiangVienDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string ChucVu { get; set; }
+        public String KhoaId { get; set; }
+        public bool TrangThai {get;set;}
+
+        public GiangVienDTO() { }
+
+        public GiangVienDTO(string id, string name, string email, DateTime? createdAt, DateTime? updatedAt, string ChucVu, string khoaId,bool TrangThai)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.CreatedAt = createdAt;
+            this.UpdatedAt = updatedAt;
+            this.ChucVu = ChucVu;
+            this.KhoaId = KhoaId;
+            this.TrangThai = TrangThai;
+        }
+    }
+}
