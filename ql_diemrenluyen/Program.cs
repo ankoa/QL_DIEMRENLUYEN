8c46709e63e2bcb4a2e6ecf082fc69c8a6cbae88
ankoa <100978052+ankoa@users.noreply.github.com>
2024-12-03 02:40:30 +0700

    Update Program.cs

--- ql_diemrenluyen/Program.cs
+++ ql_diemrenluyen/Program.cs
@@ -1,3 +1,4 @@
+using ql_diemrenluyen.GUI;
 using ql_diemrenluyen.GUI.ADMIN;
 
 namespace ql_diemrenluyen
@@ -21,7 +22,7 @@ namespace ql_diemrenluyen
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MenuAdmin());
+            Application.Run(new Login());
             //Application.Run(new chamdrl("Chấm", 4, 8));
 
             //Application.Run(new Dashboard());
